fd9d57be8eb0599df38f67206a8dcd538f11fcae
isd9194 <sachindangi942@gmail.com>
2025-04-29 19:59:59 +0530

    fix shodal

--- src/components/drawers/CustomeDrawer.js
+++ src/components/drawers/CustomeDrawer.js
@@ -1,10 +1,24 @@
 
-import { Drawer, Tooltip, } from 'antd'
+import { Drawer, Modal, Tooltip, } from 'antd'
 import { LogoutOutlined, } from "@ant-design/icons";
 import { Link } from 'react-router-dom';
 import { logoutUser } from '../../utils/Logout';
+import { useState } from 'react';
 
 export const CustomDrawer = ({ closeDrawer, isDrawerVisible }) => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    }
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+        logoutUser();
+    }
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    }
     return (
         <>
             <Drawer
@@ -18,12 +32,22 @@ export const CustomDrawer = ({ closeDrawer, isDrawerVisible }) => {
                     title="Logout Here ?"
                 >
                     <Link className='text-danger pointer'
-                        onClick={() => logoutUser()}
+                        onClick={() => showModal()}
                     >
                         Logout <LogoutOutlined />
                     </Link>
                 </Tooltip>
             </Drawer>
+            <Modal
+                title="Are you sure you want to Logout ? "
+                open={isModalVisible}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                okText="Yes"
+                cancelText="No"
+            >
+                <p>Click Yes to confirm Logout.</p>
+            </Modal>
         </>
     )
 }
